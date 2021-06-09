d7fce452c1138295683fdc251f0287055163b9cd
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-08 22:46:59 -0400

    adding functions for score storage and linking js file.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,19 @@
+// setting up variables to navigate the DOM
 var navBar = document.querySelector("nav");
 var highScore = document.getElementById("highscore");
 var timer = document.getElementById("timer2");
-var mainBox= document.getElementById("main");
+var mainBox = document.getElementById("main");
 var startBtn = document.getElementById("startbtn");
+var gameName = document.getElementsByClassName("game-name");
 var intro = document.getElementById("intro");
 var answerbtns = document.getElementsByClassName("answerbuttons");
+var input = document.getElementById("input-field");
 var ansMessage = document.getElementsById("right-wrong");
 var submitBtn = document.getElementsByID("submit-button");
 var initials = document.getElementById("initials");
 
-
-
-
-
-
-var questions = [
-    {
+// setting up questions array
+var questions = [{
         questionTitle: "Commonly used data types DO NOT include the following: ",
         choices: ["string", "booleans", "alerts", 'numbers'],
         answerkey: "alerts"
@@ -27,7 +25,7 @@ var questions = [
     },
     {
         questionTitle: "Arrays in Javascript can be used to store: ",
-        choices: ["numbers & strings", "booleans", "other arrays", "all of the above" ],
+        choices: ["numbers & strings", "booleans", "other arrays", "all of the above"],
         answerkey: "all of the above"
     },
     {
@@ -42,38 +40,136 @@ var questions = [
     }
 ];
 
-
+// setting up clock timer
 startBtn.addEventListener("click", setTime);
 
-var score= 0;
-var secondsLeft = 75;
+var score = 0;
+var question = 0;
+var secondsLeft = 50;
 var timerInterval;
+var sArray = [];
 var timeOff = 10;
 
-function setTime(){
-timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = "Timer: "+ secondsLeft + " seconds left";
+function quizStr() {
+    // sets timer
+    timer.textContent = secondsLeft;
+    setTime();
 
-    if(secondsLeft <= 0) {
-      // Stops execution of action at set interval
-    clearInterval(timerInterval);
-      // Calls function to create and append image
-    sendMessage();
-    }
+    nextQuestion();
 
+    startBtn.style.display = "none";
+}
+
+function nextQuestion() {
+    // mainBox.className =
+
+};
+
+function answers() {
+
+};
+
+function setTime() {
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = "Timer: " + secondsLeft + " seconds left";
+
+        if (secondsLeft < 1) {
+            timer.textContent = 0;
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            sendMessage();
+        };
+        if (question === 5) {
+            timer.textContent = secondsLeft;
+            clearInterval(timerInterval);
+        }
     }, 1000);
 };
 
 function sendMessage() {
-    mainEl.textContent="Game Over."
+    mainEl.textContent = "Game Over."
 };
 
-setTime();
+function highScoreLoad(event) {
+    event.preventDefault();
 
+    if (initials.nodeValue.lenght === 0) {
+        return
 
-questionSwitch.addEventListener("click", function() {
-    if (questions[1].choices !== answerkey) {
-        s
+    } else {
+        newScore = {
+            userName: initials.nodeValue.trim(),
+            userScore: score
+        };
+        sArray.push(newScore);
+
+        sArray.sort((a, b) => b.userScore - a.userScore);
+
+        localStorage.setItem('score', JSON.stringify(scoreArray));
+
+        highScoreShow();
+
     }
-});
+};
+
+function highScoreShow() {
+    storedScores = JSON.parse(localStorage.getItem('score'));
+
+    if (storedScores !== null) {
+        sArray = storedScores;
+
+        return sArray;
+    }
+};
+
+function highScorePull() {
+    if (timerInterval) {
+        clearInterval(timerInterval);
+    };
+
+    // create new list and button elements and append them to container
+
+
+
+
+
+    // hide nav (etc)
+    startBtn.style.display = "none";
+    navBar.style.visibility = "hidden";
+    gameName.textContent = "High Scores";
+    intro.textContent = "";
+    intro.setAttribute('style', 'border-top: 0');
+    answerkey.style.display = 'none';
+    input.style.display = 'none';
+
+    for (i = 0; i < sArray.length; i++) {
+        let score = sArray[i].userName + ' : ' + sArray[i].userScore;
+
+        li = document.createElement('li');
+        li.textContent = score;
+        ul.appendChild(li);
+    }
+
+    returnButton.addEventListener('click', function () {
+        location,
+        href = "index.html"
+    });
+
+    clearButton.addEventListener('click', function () {
+        localStorage.clear();
+        ul.innerHTML = "";
+    });
+
+};
+
+// function hideFirstTable() {
+
+// };
+
+// questionSwitch.addEventListener("click", function() {
+//     if (questions[1].choices !== answerkey) {
+//         s
+//     }
+// }); ;
