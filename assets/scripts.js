af8e612cefbfb7358f5c59780ac753c7d029ac63
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-09 19:44:08 -0400

    adding some bootstrap elements in to html and debugging

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,12 @@
 // setting up variables to navigate the DOM
 var navBar = document.querySelector("nav");
 var highScore = document.getElementById("highscore");
-var timer = document.getElementById("timer2");
-var mainBox = document.getElementById("main");
+var timer = document.getElementById("timer");
+var mainBox = document.getElementById("container");
 var startBtn = document.getElementById("startbtn");
-var gameName = document.getElementsByClassName("game-name");
-var intro = document.getElementById("intro");
-var answerbtns = document.getElementsByClassName("answerbuttons");
+var title = document.getElementById("title");
+var text = document.getElementById("intro");
+var answerbtns = document.getElementsByClassName("answer-button");
 var qAnswers = document.getElementById('quiz-answers');
 var input = document.getElementById("input-field");
 var ansMessage = document.getElementById("right");
@@ -15,135 +15,178 @@ var initials = document.getElementById("initials");
 
 // setting up questions array
 var questions = [{
-        questionTitle: "Commonly used data types DO NOT include the following: ",
+        title: "Commonly used data types DO NOT include the following: ",
         choices: ["string", "booleans", "alerts", 'numbers'],
         answerkey: "alerts"
     },
     {
-        questionTitle: "Conditions of an 'if/else' are enclosed within: ",
+        title: "Conditions of an 'if/else' are enclosed within: ",
         choices: ["'quotes'", "curly brackets '{}'", "parenthesis '()'", "square brackets '[]'"],
         answerkey: "curly brackets '{}'"
     },
     {
-        questionTitle: "Arrays in Javascript can be used to store: ",
+        title: "Arrays in Javascript can be used to store: ",
         choices: ["numbers & strings", "booleans", "other arrays", "all of the above"],
         answerkey: "all of the above"
     },
     {
-        questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        title: "A very useful tool used during development and debugging for printing content to the debugger is: ",
         choices: ["Javascript", "terminal bash", "for loops", "console.log"],
         answerkey: "console.log"
     },
     {
-        questionTitle: "String values must be enclosed within ______ when being assided to variables. ",
+        title: "String values must be enclosed within ______ when being assided to variables. ",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
         answerkey: "quotes"
     }
 ];
 
 // setting up clock timer
-startBtn.addEventListener("click", setTime);
-
 var score = 0;
 var theQuestions = 0;
-var secondsLeft = 50;
-var timerInterval;
+var secondsLeft = 0;
 var sArray = [];
-var timeOff = 10;
+var timerInterval = false;
+
+
 
 function quizStr() {
     // sets timer
+    secondsLeft = 50;
     timer.textContent = secondsLeft;
+    
+    // starts countdown
     setTime();
 
+    // starts question page
     nextQuestion();
 
+    // makes the start button disappear
     startBtn.style.display = "none";
-}
+};
 
+// change display to the next question
 function nextQuestion() {
-    // mainBox.className = "results-page mt-5"
-    gameName.textContent = "Question " + (questions = 1);
-    gameName.setAttribute('class', 'h2');
-    intro.textContent = questions[theQuestions].title;
-    intro.className = 'h4';
-    intro.setAttribute('style', 'border-top: 1px double #ba251a; padding-top: 20px;')
-    
-    quizAnswers.style.display = 'block';
-    
-    answerButtons[0].textContent = questions[theQuestions].choices[0];
-    answerButtons[1].textContent = questions[theQuestions].choices[1];
-    answerButtons[2].textContent = questions[theQuestions].choices[2];
-    answerButtons[3].textContent = questions[theQuestions].choices[3]
-    
+
+    // change appearance of page
+    mainBox.className = "results-page mt-5"
+    title.textContent = "Question " + (theQuestions = 1);
+    title.setAttribute('class', 'h2');
+    text.textContent = questions[theQuestions].title;
+    text.className = 'h4';
+    text.setAttribute('style', 'border-top: 1px double #ba251a; padding-top: 20px;')
+
+    // display the answer buttons
+    qAnswers.style.display = 'block';
+
+
+    // take the answer options from the current index in the questions array and assigns them one by one to each of the answer buttons.  
+    answerbtns[0].textContent = questions[theQuestions].choices[0];
+    answerbtns[1].textContent = questions[theQuestions].choices[1];
+    answerbtns[2].textContent = questions[theQuestions].choices[2];
+    answerbtns[3].textContent = questions[theQuestions].choices[3];
+
+    // clicking one of the buttons calls the anwsers () function
     for (i = 0; i < answerbtns.length; i++) {
         answerbtns[i].addEventListener('click', answers);
     }
 
 };
 
-function answers() {
+function answers(event) {
+    // checking that the button and answer values are the same
+    console.log('User chose: ' + event.target.textContent);
+    console.log('Correct answer: ' + questions[theQuestions].answer);
 
-};
+    // if selection is correct displays correct and increases score and theQuestions variables
+    if (event.target.textContent === questions[theQuestions].answerkey) {
+        ansMessage.style.display = 'block';
+        ansMessage.textContent = 'Correct!';
+        ansMessage.className = 'answer-message';
+        theQuestions++;
+        score++;
+        console.log(score);
 
-function setTime() {
-    timerInterval = setInterval(function () {
-        secondsLeft--;
-        timer.textContent = "Timer: " + secondsLeft + " seconds left";
+        // message disappears after set time
+        setTimeout(function () {
+            ansMessage.style.display = 'none';
+        }, 800);
 
-        if (secondsLeft < 1) {
-            timer.textContent = 0;
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append image
+        // end game if current question is 4
+        if (theQuestions === questions.length) {
             sendMessage();
+
+            // else go to next question
+        } else {
+            nextQuestion();
         };
-        if (theQuestions === 5) {
-            timer.textContent = secondsLeft;
-            clearInterval(timerInterval);
-        }
-    }, 1000);
+
+        // if selection is incorrect displays incorrect and decreases total time and increases theQuestions
+    } else {
+        theQuestions++;
+        ansMessage.style.display = 'block';
+        ansMessage.textContent = 'Incorrect!';
+        ansMessage.className = 'answer-message';
+
+        // message disappears after set time
+        setTimeout(function () {
+            ansMessage.style.display = 'none';
+        }, 800);
+
+        // ends game if timer is less than 10 seconds, as 10 seconds will be removed
+        if (secondsLeft < 10) {
+            secondsLeft -= 10;
+            sendMessage();
+
+            // ends game if user is on the last question
+        } else if (theQuestions === 5) {
+            sendMessage();
+
+            // else subtracts time from timer and moves onto next question
+        } else {
+            secondsLeft -= 10;
+            nextQuestion();
+        };
+    }
 };
 
 function sendMessage() {
     qAnswers.style.display = "none";
-    // mainBox.className = 'quiz-page mt-5'
-    gameName.setAttribute("class", "h2");
-    intro.setAttribute("style", "border-top: 0");
-    intro.removeAttribute("class");
-    intro.textContent = "Your Final Score is " + score + ". Enter your initials.";
+    mainBox.className = 'quiz-page mt-5'
+    title.setAttribute("class", "h2");
+    text.setAttribute("style", "border-top: 0");
+    text.removeAttribute("class");
+    text.textContent = "Your Final Score is " + score + ". Enter your initials.";
     input.style.display = "block";
 
-    if (timer <= 0) {
-        gameName.textContent = "Time's Up!";
+    if (secondsLeft <= 0) {
+        title.textContent = "Time's Up!";
 
     } else {
-        gameName.textContent = "Great Job.";
+        title.textContent = "Great Job.";
     }
 
     submitBtn.addEventListener("click", highScoreLoad);
-}
-
-
+};
 
 function highScoreLoad(event) {
     event.preventDefault();
 
-    if (initials.nodeValue.lenght === 0) {
-        return
+    if (initials.value.length === 0) {
+        return;
 
     } else {
-        newScore = {
-            userName: initials.nodeValue.trim(),
+        gameScore = {
+            userName: initials.value.trim(),
             userScore: score
         };
-        sArray.push(newScore);
+        sArray.push(gameScore);
 
         sArray.sort((a, b) => b.userScore - a.userScore);
 
-        localStorage.setItem('score', JSON.stringify(scoreArray));
+        localStorage.setItem('score', JSON.stringify(sArray));
 
-        highScoreShow();
+        highScorePull();
 
     }
 };
@@ -163,19 +206,24 @@ function highScorePull() {
         clearInterval(timerInterval);
     };
 
-    // create new list and button elements and append them to container
-
-
-
-
+    // creates new list and button elements and appends them to container
+    container.className = 'score-page mt-5 card bg-light p-4';
+    let ul = document.createElement('ul');
+    let returnButton = document.createElement('button');
+    let clearButton = document.createElement('button');
+    returnButton.textContent = 'Go Back';
+    clearButton.textContent = 'Clear High Scores';
+    container.appendChild(ul);
+    container.appendChild(returnButton);
+    container.appendChild(clearButton);
 
     // hide nav (etc)
     startBtn.style.display = "none";
     navBar.style.visibility = "hidden";
-    gameName.textContent = "High Scores";
-    intro.textContent = "";
-    intro.setAttribute('style', 'border-top: 0');
-    answerkey.style.display = 'none';
+    title.textContent = "High Scores";
+    text.textContent = "";
+    text.setAttribute('style', 'border-top: 0');
+    qAnswers.style.display = 'none';
     input.style.display = 'none';
 
     for (i = 0; i < sArray.length; i++) {
@@ -187,8 +235,7 @@ function highScorePull() {
     }
 
     returnButton.addEventListener('click', function () {
-        location,
-        href = "index.html"
+        location.href = "index.html"
     });
 
     clearButton.addEventListener('click', function () {
@@ -198,17 +245,32 @@ function highScorePull() {
 
 };
 
+function setTime() {
+    timerInterval = setInterval(function () {
+        timer.textContent = secondsLeft ;
+        secondsLeft--;
 
+        if (secondsLeft < 1) {
+            timer.textContent = 0;
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            sendMessage();
+        };
+        if (theQuestions === 5) {
+            timer.textContent = secondsLeft;
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+};
+
+startBtn.addEventListener("click", setTime);
+
+// event listener for when you click the start button
 startBtn.addEventListener('click', quizStr);
 
+// loads parsed local storage data into score array
+highScoreShow();
 
-
-// function hideFirstTable() {
-
-// };
-
-// questionSwitch.addEventListener("click", function() {
-//     if (questions[1].choices !== answerkey) {
-//         s
-//     }
-// }); ;
+// event listener for when you click highscores link in navbar
+highScore.addEventListener('click', highScorePull);
